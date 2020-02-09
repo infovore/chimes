713a025c357287a4d5d393a7d349fae766f3de3d
Tom Armitage <tom@infovore.org>
2020-02-09 18:15:47 +0000

    Wind direction, chime count.

--- chimes.lua
+++ chimes.lua
@@ -14,6 +14,36 @@ chime_radius = 22
 
 noise_offset = Vector2.new(0,0)
 
+bpm = 120
+alt = false
+
+wind_direction = 180
+wind_intensity = 10
+
+function init()
+  hammer_pos = Vector2:new(0,0)
+  
+  m = metro.init(metro_update, 6.0/bpm)
+  m:start()
+    
+  screen_m = metro.init(metro_redraw)
+  screen_m:start(1.0/60)
+  
+  redraw()
+end
+
+function key(n,z)
+  if n==1 then
+    alt = z==1
+  elseif(n == 2 and z== 1) then
+    noise_offset = Vector2:new(0,0)
+  end
+end
+
+function metro_redraw()
+  redraw()
+end
+
 function redraw()
   screen.clear()
   
@@ -24,20 +54,29 @@ function redraw()
   screen.circle(calc_hammer_pos.x, calc_hammer_pos.y, hammer_radius)
   screen.stroke()
   draw_chimes()
+  
+  draw_wind()
+
+  screen.move(0,10)
+  screen.text(bpm)
+  
   screen.update()
 end
 
-function init()
-  hammer_pos = Vector2:new(0,0)
-  
-  m = metro.init(metro_redraw,0.1)
-  m:start()
-  redraw()
-end
-
-function key(n,d)
-  if(n == 2 and d== 1) then
-    noise_offset = Vector2:new(0,0)
+function enc(n,d)
+  if(n==1) then
+    bpm = bpm+d
+    m.time = 6.0/bpm
+  elseif (n==2) then
+    print(alt)
+    if alt then
+      chime_count = util.clamp(chime_count + d, 3, 12)
+    else
+      wind_direction = (wind_direction + d) % 360
+    end
+  elseif (n==3) then
+    wind_intensity = (wind_intensity + d)
+    wind_intensity = util.clamp(wind_intensity,2,15)
   end
 end
 
@@ -45,12 +84,10 @@ function rand()
   return 2.0 * math.random()-1
 end
 
-function metro_redraw()
+function metro_update()
   random_vector = Vector2:new_from_deg_and_intensity(math.random()*360, math.random()*chime_radius)
   
   noise_offset = random_vector
-
-  redraw()
 end
 
 function draw_chimes()
@@ -65,4 +102,30 @@ function draw_chimes()
     screen.circle(calc_chime_pos.x, calc_chime_pos.y, chime_circle_radius)
     screen.fill()
   end
+end
+
+function draw_wind()
+  arrow_length = 10.0
+  
+  arrow_end = Vector2:new(0,wind_intensity/2)
+  arrow_tip = Vector2:new(0,0-(wind_intensity/2))
+  blade_1 = Vector2:new(-3,(0-(wind_intensity/2))+3)
+  blade_2 = Vector2:new(3,(0-(wind_intensity/2))+3)
+  
+  offset = Vector2:new(52,20)
+  
+  arrow_tip = arrow_tip:rotate(0-wind_direction):add(screen_center):add(offset)
+  arrow_end = arrow_end:rotate(0-wind_direction):add(screen_center):add(offset)
+  blade_1 = blade_1:rotate(0-wind_direction):add(screen_center):add(offset)
+  blade_2 = blade_2:rotate(0-wind_direction):add(screen_center):add(offset)
+  
+  screen.move(arrow_end.x, arrow_end.y)
+  screen.line(arrow_tip.x, arrow_tip.y)
+  screen.line(blade_1.x,blade_1.y)
+  screen.move(arrow_tip.x, arrow_tip.y)
+  screen.line(blade_2.x, blade_2.y)
+  screen.stroke()
+  
+  screen.move(128,44)
+  screen.text_right((wind_direction+180) % 360)
 end
